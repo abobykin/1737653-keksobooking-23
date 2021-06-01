059af60fe8d47e73814e78bda40f20ecf2965ac5
Alexander Bobykin <birmudo@gmail.com>
2021-06-01 23:18:33 +0600

    2.14. Нужно больше функций - Правки после ревью

--- js/main.js
+++ js/main.js
@@ -5,8 +5,9 @@
 function getRandomInteger(from, to) {
   if (from < to) {
     return Math.floor(Math.random() * (to - from + 1)) + from;
+  }  else {
+    return Math.floor(Math.random() * (from - to + 1)) + to;
   }
-  return 'Error: Invalid range';
 }
 
 getRandomInteger(1, 10);
@@ -16,8 +17,9 @@ getRandomInteger(1, 10);
 function getRandomFloat(from, to, decimals) {
   if (from < to) {
     return Number((Math.random() * (to - from) + from).toFixed(decimals));
+  } else {
+    return Number((Math.random() * (from - to) + to).toFixed(decimals));
   }
-  return 'Error: Invalid params';
 }
 
 getRandomFloat(1.432, 10.21, 3);
