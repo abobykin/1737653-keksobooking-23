a14c54f205b3083df52b83c107da783f40b7dac2
Alexander Bobykin <birmudo@gmail.com>
2021-06-19 15:44:59 +0600

    Небольшой фикс

--- js/main.js
+++ js/main.js
@@ -9,4 +9,5 @@ setTimeout(setFormEnabled, 9000);
 // ЗАМЕТКА: Ниже временное решение для того чтобы удобнее видеть на карте несколько объявлений
 // для задания 'Отрисуй меня полностью (часть 1)' (вместо console.log)
 mapCanvas.style.display = 'flex';
+
 mapCanvas.appendChild(popupFragment);
